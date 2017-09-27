a7dc1979d72dfd64ba0a590319fedc0b974c775b
Andrea Zucchini <azga84@gmail.com>
2017-09-27 19:01:49 +0200

    test form rss match

---
+++ matchers/rss_test.go
@@ -0,0 +1,22 @@
+package matchers
+
+import (
+	"testing"
+	"github.com/zucchinidev/golangSearchEngine/search"
+)
+
+func TestRSSMatcher_Search(t *testing.T) {
+	matcher := new(RSSMatcher)
+	feed := search.Feed{
+		Name: "npr",
+		URI:  "http://www.npr.org/rss/rss.php?id=1001",
+		Type: "rss",
+	}
+	results, err := matcher.Search(&feed, "Between Blacks")
+	if err != nil {
+		t.Error("should retrieve a match string but retrieve an error", err)
+	}
+	if len(results) == 0 {
+		t.Error("should retrieve a match string but it doesn't return anything")
+	}
+}
