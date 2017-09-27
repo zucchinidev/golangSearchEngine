aff3d2df5ee8287a905c553874f880f0784ef219
Andrea Zucchini <azga84@gmail.com>
2017-09-27 20:15:53 +0200

    creation of the main method in the search logic. Perform the search concurrently.

--- search/search.go
+++ search/search.go
@@ -1,8 +1,42 @@
 package search
 
-import "log"
+import (
+	"log"
+	"sync"
+)
 
 var availableMatchers = make(map[string]Matcher)
+
+func Run(searchTerm string) {
+	feeds, err := GetFeeds()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resultChannel := make(chan *Result)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(feeds))
+
+	for _, feed := range feeds {
+		matcher, exists := availableMatchers[feed.Type]
+		if !exists {
+			matcher = availableMatchers[DefaultMatcherTypeName]
+		}
+
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchTerm, resultChannel)
+			waitGroup.Done()
+		}(matcher, feed)
+	}
+
+	go func() {
+		waitGroup.Wait()
+		close(resultChannel)
+	}()
+
+	DisplayResults(resultChannel)
+}
+
 
 func Register(matcherType string, matcher Matcher)  {
 	if _, exists := availableMatchers[matcherType]; exists {
