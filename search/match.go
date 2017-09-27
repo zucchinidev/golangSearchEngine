24d73706f905d4f59273a766c807854d71b5ae80
Andrea Zucchini <azga84@gmail.com>
2017-09-27 19:09:13 +0200

    create match function, use a matcher interface

--- search/match.go
+++ search/match.go
@@ -1,10 +1,24 @@
 package search
 
+import "log"
+
 type Result struct {
-	Field string
+	Field   string
 	Content string
 }
 
 type Matcher interface {
 	Search(feed *Feed, searchTerm string) ([]*Result, error)
+}
+
+func Match(matcher Matcher, feed *Feed, searchTerm string, resultChannel chan<- *Result) {
+	foundResults, err := matcher.Search(feed, searchTerm)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for _, result := range foundResults {
+		resultChannel <- result
+	}
 }
