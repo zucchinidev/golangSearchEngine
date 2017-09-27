2a97ac85e743044b3e39b8ed890683c82274b860
Andrea Zucchini <azga84@gmail.com>
2017-09-27 17:33:49 +0200

    retrieve feeds from file

--- search/feed.go
+++ search/feed.go
@@ -1,7 +1,30 @@
 package search
 
+import (
+	"os"
+	"encoding/json"
+	"path/filepath"
+)
+
+const dataFile = "../data/feeds.json"
+
 type Feed struct {
 	Name string `json:"site"`
 	URI  string `json:"link"`
 	Type string `json:"type"`
+}
+
+func GetFeeds() ([]*Feed, error) {
+	absolutePath, err := filepath.Abs(dataFile)
+	if err != nil {
+		return nil, err
+	}
+	file, err := os.Open(absolutePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+	return feeds, nil
 }
